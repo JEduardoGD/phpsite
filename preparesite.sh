20975c0df7e29405cd240d59113b650e2bd6c63b
tesorero@fmre.mx <tesorero@fmre.mx>
2024-09-13 13:43:08 -0600

    fix, adding start-apache

--- preparesite.sh
+++ preparesite.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-source env.properties
+export $(grep -v '^#' env.properties | xargs)
 rm -f src/db.php
 echo "<?php" >> src/db.php
-echo "    \$servername = localhost:${MYSQL_PORT}" >> src/db.php
-echo "    \$username = ${MYSQL_USER}" >> src/db.php
-echo "    \$password = ${MYSQL_PASSWORD}" >> src/db.php
-echo "    \$dbname = ${vMYSQL_DATABASE}" >> src/db.php
+echo "    \$servername = \"localhost:${MYSQL_PORT}\"" >> src/db.php
+echo "    \$username = \"${MYSQL_USER}\"" >> src/db.php
+echo "    \$password = \"${MYSQL_PASSWORD}\"" >> src/db.php
+echo "    \$dbname = \"${MYSQL_DATABASE}\"" >> src/db.php
 echo "    // Create connection" >> src/db.php
 echo "    \$conn = mysqli_connect(\$servername, \$username, \$password, \$dbname);" >> src/db.php
 echo "    // Check connection" >> src/db.php
 echo "    if (!\$conn) {" >> src/db.php
-echo "        die("Connection failed: " . mysqli_connect_error());" >> src/db.php
+echo "        die(\"Connection failed: \" . mysqli_connect_error());" >> src/db.php
 echo "    }" >> src/db.php
 echo "?>" >> src/db.php
+docker build -t phpapp .
